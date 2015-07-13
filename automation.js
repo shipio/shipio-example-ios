3c09d2f48c5033f1af062eb087d8c12960a9102c
Eric Black <eric@ship.io>
2015-07-13 11:15:11 -0700

    Added some logging and screenshots to UIAutomation test.

--- automation.js
+++ automation.js
@@ -1,6 +1,7 @@
 var target = UIATarget.localTarget();
 var app = target.frontMostApp();
 var window = app.mainWindow();
+UIALogger.logMessage("Starting to do some logging.");
 window.buttons()["Tap Me"].tap();
-
+target.captureScreenWithName("afterTapScreenShot");
 //Added comment
